933cb0ccd6681eff33006dd1183235b5335461d3
Tom Lea <contrib@tomlea.co.uk>
2008-10-10 02:38:46 +0100

    A little refactoring

--- lib/active_record/lint/memoize.rb
+++ lib/active_record/lint/memoize.rb
@@ -1,5 +1,6 @@
 unless defined? Memoize
   module Memoize
+
     def self.included(other)
       other.extend(ClassMethods)
     end
@@ -8,16 +9,19 @@ unless defined? Memoize
       def memoize(*methods)
         methods.each do |method|
           define_method "#{method}_with_memoize" do |*args|
-            unless store = instance_variable_get("@_#{method}_memoized")
-              instance_variable_set("@_#{method}_memoized", store = {})
-            end
-            
-            store[args] ||= send("#{method}_without_memoize", *args)
+             memoize_store_for(method)[args] ||= send("#{method}_without_memoize", *args)
           end
 
           alias_method_chain method, :memoize          
         end
       end
     end
+
+    def memoize_store_for(method)
+      unless store = instance_variable_get("@_#{method}_memoized")
+        instance_variable_set("@_#{method}_memoized", store = {})
+      end
+    end
   end
+
 end
